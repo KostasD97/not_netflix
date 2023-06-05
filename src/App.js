ab062773e712eea94d64b094e25343378c776d3b
KostasD97 <kostasd.777@gmail.com>
2023-06-05 19:20:19 +0300

    fix of movie list and adding bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 //import { useState } from "react";
 
 // Radio Button ready
+
 // function App() {
 //   const [section, setSection] = useState("none");
 //   const onOptionChange = (e) => {
@@ -40,7 +41,9 @@ import logo from "./logo.svg";
 // }
 
 import React, { useState } from "react";
-import MovieList from "./components/MovieList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList.js";
 
 // second task using list of movies...
 const App = () => {
@@ -69,10 +72,68 @@ const App = () => {
       Poster:
         "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
     },
+    {
+      Title: "Star Wars: Episode VII - The Force Awakens",
+      Year: "2015",
+      imdbID: "tt2488496",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode I - The Phantom Menace",
+      Year: "1999",
+      imdbID: "tt0120915",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode III - Revenge of the Sith",
+      Year: "2005",
+      imdbID: "tt0121766",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode II - Attack of the Clones",
+      Year: "2002",
+      imdbID: "tt0121765",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
+    },
+    {
+      Title: "Rogue One: A Star Wars Story",
+      Year: "2016",
+      imdbID: "tt3748528",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VIII - The Last Jedi",
+      Year: "2017",
+      imdbID: "tt2527336",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode IX - The Rise of Skywalker",
+      Year: "2019",
+      imdbID: "tt2527338",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg",
+    },
   ]);
   return (
-    <div>
-      <MovieList movies={movies} />
+    <div className="container-fluid">
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 };
